bc30bb6d5714fb4cdaacb9879bfe355f9bed13bb
Yifan Sun <yifan.sun@emc.com>
2016-10-18 22:36:29 -0400

    An install script

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+backup_folder_name=$HOME/old_dotfiles/$(date +%s)_old_dotfiles
+mkdir -p $backup_folder_name
+
+mv $HOME/.vimrc $backup_folder_name 2> /dev/null
+mv $HOME/.vim $backup_folder_name 2> /dev/null
+mv $HOME/.tmux.conf $backup_folder_name 2> /dev/null
+
+ln -s $PWD/vimrc $HOME/.vimrc
+ln -s $PWD/vim $HOME/.vim
+ln -s $PWD/tmux.conf $HOME/.tmux.conf 
