32193752f8a451f35b96e720db73fd48d5de59d1
Yifan Sun <yifan.sun@emc.com>
2016-10-25 11:14:01 -0400

    doxygen, m4, ycm

--- install.sh
+++ install.sh
@@ -8,17 +8,19 @@ mv $HOME/.vim $backup_folder_name 2> /dev/null
 mv $HOME/.tmux.conf $backup_folder_name 2> /dev/null
 mv $HOME/.zshrc $backup_folder_name 2> /dev/null
 
-
 sudo apt-get update
-sudo apt-get install git zsh
+sudo apt-get install git zsh vim 
 
 oh-my-zsh/tools/install.sh
-chsh $USER -s `which zsh`
 
-git submodule init
-git submodule update
+git submodule update --init --recursive
+
+# Install YouCompleteMe with clang
+cd vim/bundle/youcompleteme/ && ./install.py --clang-completer && cd ../../..
 
 ln -s $PWD/vimrc $HOME/.vimrc
 ln -s $PWD/vim $HOME/.vim
 ln -s $PWD/tmux.conf $HOME/.tmux.conf 
 ln -s $PWD/zshrc $HOME/.zshrc
+
+chsh $USER -s `which zsh`
