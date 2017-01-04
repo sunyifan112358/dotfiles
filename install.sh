31af7efc65e9298c71b3a566631233fe8a05fb27
Yifan Sun <sunyifan112358@gmail.com>
2017-01-03 19:22:53 -0500

    install vscode config

--- install.sh
+++ install.sh
@@ -9,6 +9,7 @@ mv $HOME/.vimrc $backup_folder_name 2> /dev/null
 mv $HOME/.vim $backup_folder_name 2> /dev/null
 mv $HOME/.tmux.conf $backup_folder_name 2> /dev/null
 mv $HOME/.zshrc $backup_folder_name 2> /dev/null
+mv $HOME/.config/Code/User/settings.json $backup_folder_name 2> /dev/null
 
 sudo add-apt-repository ppa:webupd8team/java 
 sudo apt-get update
@@ -24,5 +25,6 @@ ln -s $PWD/vimrc $HOME/.vimrc
 ln -s $PWD/vim $HOME/.vim
 ln -s $PWD/tmux.conf $HOME/.tmux.conf 
 ln -s $PWD/zshrc $HOME/.zshrc
+ln -s $PWD/code/settings.json $HOME/.config/Code/User/settings.json
 
 chsh $USER -s `which zsh`
