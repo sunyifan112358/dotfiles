ba3f0db2425e47d981289c71a5086c891077976e
Yifan Sun <sunyifan112358@gmail.com>
2017-05-13 21:54:15 -0400

    Update in touchpad setting

--- touchpad_setting.sh
+++ touchpad_setting.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
-synclient FingerHigh=150
-synclient FingerLow=100
+synclient FingerHigh=200 FingerLow=10
 synclient PalmDetect=1
+synclient PalmMinWidth=1
+synclient PalmMinZ=1
 synclient TapAndDragGesture=0
-synclient MaxTapMove=0
+synclient MaxTapMove=1
+synclient MaxTapTime=80
+
+syndaemon -i 0.8 -t -K -R
+
+
