44c087e746d0ace1b003898bba660fcaeae13cb6
Yifan Sun <sunyifan112358@gmail.com>
2017-05-14 20:34:55 -0400

    Deamonlize sycdaemon

--- touchpad_setting.sh
+++ touchpad_setting.sh
@@ -5,8 +5,8 @@ synclient PalmMinWidth=1
 synclient PalmMinZ=1
 synclient TapAndDragGesture=0
 synclient MaxTapMove=1
-synclient MaxTapTime=80
+synclient MaxTapTime=180
 
-syndaemon -i 0.8 -t -K -R
+syndaemon -i 0.8 -t -K -R -d
 
 
