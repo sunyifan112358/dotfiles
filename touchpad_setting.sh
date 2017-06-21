a0d66aaf2ee1a00fdc6f95e3971d2723294bcf90
Yifan Sun <sunyifan112358@gmail.com>
2017-06-20 22:37:34 -0400

    Adjusting the touchpad settings

--- touchpad_setting.sh
+++ touchpad_setting.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-synclient FingerHigh=200 FingerLow=10
+synclient FingerHigh=80 FingerLow=50
 synclient PalmDetect=1
-synclient PalmMinWidth=1
-synclient PalmMinZ=1
+synclient PalmMinWidth=8
+synclient PalmMinZ=20
 synclient TapAndDragGesture=0
-synclient MaxTapMove=1
+synclient MaxTapMove=10
 synclient MaxTapTime=180
 
 syndaemon -i 0.8 -t -K -R -d
