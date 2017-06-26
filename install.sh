11d57f01dda11b63d7f561666cd0e4a3307336ac
Yifan Sun <sunyifan112358@gmail.com>
2017-06-26 13:26:54 -0400

    Do not overwrite vscode settings

--- install.sh
+++ install.sh
@@ -8,12 +8,9 @@ mkdir -p $backup_folder_name
 mv $HOME/.vimrc $backup_folder_name 2> /dev/null
 mv $HOME/.vim $backup_folder_name 2> /dev/null
 mv $HOME/.tmux.conf $backup_folder_name 2> /dev/null
-mv $HOME/.zshrc $backup_folder_name 2> /dev/null
-mv $HOME/.config/Code/User/settings.json $backup_folder_name 2> /dev/null
 
 sudo apt-get -y install git vim cmake tmux build-essential python-dev
 
 ln -s $PWD/vimrc $HOME/.vimrc
 ln -s $PWD/vim $HOME/.vim
 ln -s $PWD/tmux.conf $HOME/.tmux.conf 
-ln -s $PWD/code/settings.json $HOME/.config/Code/User/settings.json
