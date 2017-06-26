afbf1bc03b0776053b1656c8392ff7a569ea9698
Yifan Sun <sunyifan112358@gmail.com>
2017-06-26 13:40:17 -0400

    using the .profile file

--- install.sh
+++ install.sh
@@ -8,9 +8,11 @@ mkdir -p $backup_folder_name
 mv $HOME/.vimrc $backup_folder_name 2> /dev/null
 mv $HOME/.vim $backup_folder_name 2> /dev/null
 mv $HOME/.tmux.conf $backup_folder_name 2> /dev/null
+mv $HOME/.profile $backup_folder_name 2> /dev/null
 
 sudo apt-get -y install git vim cmake tmux build-essential python-dev
 
 ln -s $PWD/vimrc $HOME/.vimrc
 ln -s $PWD/vim $HOME/.vim
 ln -s $PWD/tmux.conf $HOME/.tmux.conf 
+ln -s $PWD/profile $HOME/.profile
